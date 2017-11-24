6f6a04f0040d3943f5b56550ae4763cf79e65bf6
Marian Grgic <marian.grgic@korcula-hotels.com>
2017-11-24 07:56:27 +0100

    added comments to scripts

--- javascript/weather.js
+++ javascript/weather.js
@@ -7,8 +7,12 @@ function round(value, decimals) {
 
 weather = {
 
+    // object configuration variables
     containerId: "section#weather",
     iconUrl: 'https://www.metaweather.com/static/img/weather/png/64/',
+    searchInput: 'form#searchForm input[name="term"]',
+
+    // allowed locations with appropriate woeid-s
     locations: {
         0: {name: 'DUBLIN', id: '560743'},
         1: {name: 'LONDON', id: '44418'},
@@ -17,22 +21,26 @@ weather = {
         4: {name: 'VANCUVER', id: '9807'},
         5: {name: 'ISTANBUL', id: '2344116'},
     },
-    searchInput: 'form#searchForm input[name="term"]',
 
+    // init function that is called by default to load default data
     showDefault: function(){
 
+        // assign
         var $this = this;
         var $container = $(this.containerId);
         var $searchInput = $(this.searchInput);
 
+        // clear containers
         $container.html('');
         $searchInput.val('');
 
+        // load data
         $.each($this.locations, function(i, v){
             $this.loadWeather(v.id, 'home');
         });
     },
 
+    // ajax request against weather API
     loadWeather: function(code, type){
 
         var $this = this;
@@ -43,25 +51,36 @@ weather = {
             data: 'command=location&woeid=' + code,
             success:function(r){
 
+                // if there is error fetching data, display error message and return
+                if(typeof r.error !== 'undefined'){
+                    alert(r.error);
+                    return;
+                }
+
                 $this.displayWeather(r, type);
             }
         });
     },
 
+    // function to perform search
     search: function(){
 
+        // assign and load data
         var $this = this;
         var $container = $(this.containerId);
         var term = $(this.searchInput).val();
+        var found = false;
 
+        // if term is not defined or empty, return
         if(typeof term === 'undefined' || term === '' || term === null){
             alert('please Enter valid Term');
             return;
         }
 
+        // sanitize data, transform term to uppercase to allow case insensitive functionality
         var termUpper = term.toUpperCase();
-        var found = false;
 
+        // loop through allowable locations to load data i location is found
         $.each($this.locations, function(i, v){
             if(v.name == termUpper){
                 $this.loadWeather(v.id, 'details');
@@ -70,12 +89,17 @@ weather = {
             }
         });
 
+        // if not found display error message
         if(!found)
             $container.html('There are no results for search term: "' + term + '"');
     },
 
+    // display function that will generate html output
+    // @data - object - containing data
+    // @type - string - case of which type of template should be generated
     displayWeather: function(data, type){
 
+        // assign
         var $this = this;
         var $container = $(this.containerId);
 
@@ -83,6 +107,7 @@ weather = {
 
             case 'home':
 
+                // parse data
                 var title = data.title;
                 var icon = $this.iconUrl + data.consolidated_weather[0].weather_state_abbr + '.png';
                 var temperature = round(data.consolidated_weather[0].the_temp, 2);
@@ -90,6 +115,7 @@ weather = {
                 var max_temperature = round(data.consolidated_weather[0].max_temp, 2);
                 var code = data.woeid;
 
+                // add data to html template
                 var html = '<div onClick="weather.loadWeather(\'' + code + '\', \'details\');">' +
                     '<h1>' + title + ' Today</h1>' +
                     '<img src="' + icon + '" alt=""/>' +
@@ -100,17 +126,18 @@ weather = {
                     '</ul>' +
                     '</div>';
 
+                // display html
                 $container.append(html);
 
                 break;
 
             case 'details':
 
+                // parse data
                 var title = data.title;
-
-
                 var html = '<h1>Weather Details for: ' + title + '</h1>';
 
+                // iterate through data array to parse and generate html output
                 $.each(data.consolidated_weather, function(i, v){
 
                     var date = data.consolidated_weather[i].applicable_date;
@@ -140,6 +167,7 @@ weather = {
                         '</div>';
                 });
 
+                // display html
                 $container.html(html);
 
                 break;
@@ -149,6 +177,7 @@ weather = {
 
 $(document).ready(function(){
 
+    // init call
     weather.showDefault();
 
 });
